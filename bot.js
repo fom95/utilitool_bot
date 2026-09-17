949542780060dd5266e113d318fb2e98a0bd12fe
fom95 <freeoffermail95@gmail.com>
2026-09-17 00:33:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -461,7 +461,7 @@ function photoMenu() {
 
 function cropMenu(sessionId) {
     const cropUrl =
-        `https://t.me/utilitool_bot/set_photo?startapp=${encodeURIComponent(sessionId)}`;
+        `https://t.me/utilitool_bot?startapp=${encodeURIComponent(sessionId)}`;
 
     return {
         text:
